bbf01ba9059424e7623cc6d1eb46466803449f4c
rywilly <ryanj@redy-project.org>
2016-05-25 20:20:33 -0600

    Deleted all files

--- bindredy.bat
+++
@@ -1,4 +0,0 @@
-eubind ".\projects\RedyCode\source\RedyExec.exw" -I ".\redylib\dev" -icon "redy_icon.ico"
-eubind ".\projects\RedyCode\source\RedyCode.exw" -I ".\redylib\dev" -icon "redy_icon.ico"
-move ".\projects\RedyCode\source\RedyExec.exe" ".\RedyExec.exe"
-move ".\projects\RedyCode\source\RedyCode.exe" ".\RedyCode.exe"
