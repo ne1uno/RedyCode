76defaa79dcce1edec421f90b4f64ab58ecd6379
rywilly <ryanj@redy-project.org>
2016-07-05 20:08:37 -0600

    added -eudir to bat file
    added -eudir ".\euphoria" so bat file works when euphoria 4.0.5 is installed.

--- bindredy.bat
+++ bindredy.bat
@@ -1,6 +1,6 @@
 color 1F
 rem Rebuilding RedyCode.exe from .\projects\RedyCode\source\RedyCode.exw...
 
-.\euphoria\bin\eubind.exe ".\projects\RedyCode\source\RedyCode.exw" -icon ".\redy_icon.ico" -i ".\euphoria\include"
+.\euphoria\bin\eubind.exe ".\projects\RedyCode\source\RedyCode.exw" -icon ".\redy_icon.ico" -i ".\euphoria\include" -eudir ".\euphoria"
 
 move ".\projects\RedyCode\source\RedyCode.exe" ".\RedyCode.exe"
